9ffda357043f1da78e8e2685ad8ce8e21a1db1d8
e <786313105@qq.com>
2018-03-20 22:00:09 +0800

    ss

--- 作业/test2.py
+++ 作业/test2.py
@@ -1,19 +1,19 @@
-import shutil, uuid,time,threading,queue
+import sipping
 
-def copy_file1():
-    for i in range(250):
-        shutil.copyfile(r'C:\Users\Administrator\Desktop\xzrs\111.jpg', r'C:\Users\Administrator\Desktop\xzrs_pic\111'+str(uuid.uuid1())+'.jpg')
-
-if __name__=="__main__":
-    start = time.time()
-
-    multi_threads1 = []
-
-    for i in range(20):
-        m = threading.Thread(target=copy_file1(), args=())
-        multi_threads1.append(m)
-        m.start()
-    for i in multi_threads1:
-        i.join()
-    print(time.time()-start)
-    # print(type(uuid.uuid1()))
+# req = sipping.Request()
+# print(req)
+# class Test:
+#     a = 6
+#     def __init__(self):
+#         self.j = 6
+#     def yy(self):
+#         print(self.a)
+# class S_test(Test):
+#     a = 7
+#     def __init__(self):
+#         super(S_test, self).__init__()
+#
+# s = S_test()
+# s.yy()
+print(dir(sipping))
+print(help(sipping))
